9142bad793bce300ac1a6c92c24d269691d0f2ea
Alex Wu <alexdwu86@gmail.com>
2016-03-16 21:38:38 -0700

    remove some visibilityFilter stuff

--- containers/VisibleTodoList.js
+++ containers/VisibleTodoList.js
@@ -2,20 +2,21 @@ import { connect } from 'react-redux'
 import { toggleTodo } from '../actions'
 import TodoList from '../components/TodoList'
 
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case 'SHOW_ALL':
-      return todos
-    case 'SHOW_COMPLETED':
-      return todos.filter(t => t.completed)
-    case 'SHOW_ACTIVE':
-      return todos.filter(t => !t.completed)
-  }
-}
+// const getVisibleTodos = (todos, filter) => {
+//   switch (filter) {
+//     case 'SHOW_ALL':
+//       return todos
+//     case 'SHOW_COMPLETED':
+//       return todos.filter(t => t.completed)
+//     case 'SHOW_ACTIVE':
+//       return todos.filter(t => !t.completed)
+//   }
+// }
 
 const mapStateToProps = (state) => {
   return {
-    todos: getVisibleTodos(state.todos, state.visibilityFilter)
+    // todos: getVisibleTodos(state.todos, state.visibilityFilter)
+    todos: state.todos,
   }
 }
 
